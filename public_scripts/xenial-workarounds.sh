ed25285b57a55e1d12b96fda60143c71f0f62399
Ben Place <bplace@visionit.com>
2016-06-30 16:03:05 -0400

    Added the Vagrant/Ansible workarounds for Xenial.

--- public_scripts/xenial-workarounds.sh
+++ public_scripts/xenial-workarounds.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
-# install python 2
-apt install -y python
+PYTHON_REQUIRED_VERSION='Python 2.7.11+'
 
-# install aptitude
-apt install -y aptitude
+echo "Checking aptitude"
+command -v aptitude || sudo apt install -y aptitude
+
+# install python 2
+echo "Checking python version, "
+DEFAULT_CURRENT_PYTHON_VERSION=$(python --version 2>&1)
+if [ "${DEFAULT_CURRENT_PYTHON_VERSION}" != "${PYTHON_REQUIRED_VERSION}" ]
+then
+	echo "Installing python via aptitude, since we only have ${DEFAULT_CURRENT_PYTHON_VERSION}"
+	aptitude install -y python
+else
+	echo "Don't need to install python 2.7"
+fi
