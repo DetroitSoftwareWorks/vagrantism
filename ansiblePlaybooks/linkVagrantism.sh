2357a1eb3b889ff89c08893032b75544963fdc78
Ben Place <ben@detroitsoftwareworks.com>
2016-10-02 12:07:51 -0400

    Created a script to let downstream adopters of these playbooks easily integrate them into private ansible structures.

---
+++ ansiblePlaybooks/linkVagrantism.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+if [[ $# != 1 ]]
+then
+    echo "You need one argument: the path to your downstream ansbile playbooks."
+    exit
+fi
+
+TARGET_DIR=$1
+MAIN_DIR=$(dirname $0)
+
+
+if [[ ! -e ${TARGET_DIR}/site.yml ]]
+then
+    cp ${MAIN_DIR}/site.yml ${TARGET_DIR}/site.yml
+else
+    echo "File exists ($(ls ${TARGET_DIR}/site.yml)), did not overwrite."
+fi
+
+for ELEM in $(ls ${MAIN_DIR})
+do
+    if [[ -d ${ELEM} ]]
+    then
+        if [[ -e ${TARGET_DIR}/${ELEM} ]]
+        then
+            echo "Old ${ELEM}: $(ls -d ${TARGET_DIR}/${ELEM})"
+        else
+            ln -s $(pwd)/${ELEM} ${TARGET_DIR}/${ELEM}
+        fi
+    fi
+done
