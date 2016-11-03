e801cff02b385a5dfd974c0ff6bc8f54907837e1
Ben Place <benp313@users.noreply.github.com>
2016-11-03 15:41:19 -0400

    Fixed errors in link creation, and cleaned up.

--- ansiblePlaybooks/linkVagrantism.sh
+++ ansiblePlaybooks/linkVagrantism.sh
@@ -6,26 +6,25 @@ then
     exit
 fi
 
-TARGET_DIR=$1
-MAIN_DIR=$(dirname $0)
+TARGET_DIR="$1"
+MAIN_DIR=$(dirname "$0")
 
-
-if [[ ! -e ${TARGET_DIR}/site.yml ]]
+if [[ ! -e "${TARGET_DIR}/site.yml" ]]
 then
-    cp ${MAIN_DIR}/site.yml ${TARGET_DIR}/site.yml
+    cp "${MAIN_DIR}/site.yml" "${TARGET_DIR}/site.yml"
 else
-    echo "File exists ($(ls ${TARGET_DIR}/site.yml)), did not overwrite."
+    echo "File exists ($(ls "${TARGET_DIR}/site.yml")), did not overwrite."
 fi
 
-for ELEM in $(ls ${MAIN_DIR})
+for ELEM in $(ls "${MAIN_DIR}")
 do
-    if [[ -d ${MAIN_DIR}/${ELEM} ]]
+    if [[ -d "${MAIN_DIR}/${ELEM}" ]]
     then
-        if [[ -e ${TARGET_DIR}/${ELEM} ]]
+        if [[ -e "${TARGET_DIR}/${ELEM}" ]]
         then
             echo "Old ${ELEM}: $(ls -d ${TARGET_DIR}/${ELEM})"
         else
-            ln -s $(pwd)/${ELEM} ${TARGET_DIR}/${ELEM}
+            ln -s "$(pwd)/${MAIN_DIR}/${ELEM}" "${TARGET_DIR}/${ELEM}"
         fi
     fi
 done
